9cc04e14630c701548b4ce82f29ee8f96777adb3
cypherdream <cypherdream@example.com>
2026-07-02 16:40:24 +0300

    feat: implement hilt dependency injection module

---
+++ app/src/main/java/com/architecture/master/di/DatabaseModule.kt
@@ -0,0 +1,18 @@
+package com.architecture.master.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+    
+    @Provides
+    @Singleton
+    fun provideDatabase(): String {
+        return "DatabaseInstance"
+    }
+}
