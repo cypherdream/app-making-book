be2e72aa823ae9d9ff555487e75083ac25fa6725
cypherdream <cypherdream@example.com>
2026-07-24 14:16:34 +0300

    Fix dependencies - use kotlinx.serialization instead of Gson, enable BuildConfig

--- app-making-book-commercial/android-app/build.gradle.kts
+++ app-making-book-commercial/android-app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application") version "8.5.2"
     id("org.jetbrains.kotlin.android") version "1.9.24"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.24"
     id("com.google.dagger.hilt.android") version "2.51.1"
     id("com.google.devtools.ksp") version "1.9.24-1.0.20"
 }
@@ -15,6 +16,11 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+        buildConfigField("String", "BASE_URL", "\"https://10.0.2.2:3000/\"")
+    }
+
+    buildFeatures {
+        buildConfig = true
     }
 
     buildTypes {
@@ -45,8 +51,9 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.51.1")
     ksp("com.google.dagger:hilt-android-compiler:2.51.1")
 
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
     implementation("androidx.room:room-runtime:2.6.1")
